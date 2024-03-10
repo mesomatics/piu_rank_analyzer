4a5fcf38ad723c358bcf989417bd6a2f63b190ec
mesomatics <68718172+mesomatics@users.noreply.github.com>
2024-03-10 16:14:23 +0900

    Add files via upload

---
+++ src/backend/analyzer/single_month.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+from ..config import *
+
+
+class SingleMonthAnalyzer:
+    def level_preference(self, df, query=None):
+        """
+        Parameters
+        ----------
+        df: DataFrame
+        
+        query: str, optional
+        """
+        if query is not None:
+            df = df.query(query)
+        def count_(mode):
+            df_sub = df.query(f"`{COL_MODE}` == @mode")
+            count = df_sub.groupby(COL_LEVEL)[COL_COUNT].sum()
+            count.name = mode
+            return count
+        count_s = count_("S")
+        count_d = count_("D")
+        count_all = pd.concat([count_s, count_d], axis=1)
+        count_all = count_all.fillna(0).astype(int)
+        count_all["ALL"] = count_all.sum(axis=1)
+        return count_all
+
+    def song_preference(self, df, query=None):
+        df = df.copy()
+        if query is not None:
+            df = df.query(query)
+        df["Pref_Chart"] = df.groupby([COL_LEVEL, COL_MODE])[COL_COUNT].rank(ascending=False, pct=True)
+        df["Pref_Song"] = df.groupby(COL_TITLE)["Pref_Chart"].transform("mean")
+        pref_song = df.groupby(COL_TITLE)["Pref_Chart"].mean()
+        return pref_song, df
