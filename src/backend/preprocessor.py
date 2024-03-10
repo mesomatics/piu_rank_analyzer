97cd38e22d0d98a2582f17e0c0a65a73c4e81b93
mesomatics <68718172+mesomatics@users.noreply.github.com>
2024-03-10 16:24:31 +0900

    Update preprocessor.py

--- src/backend/preprocessor.py
+++ src/backend/preprocessor.py
@@ -1,12 +1,10 @@
 class PiuRankPreprocessor:
-    """
-    Not Implemented
-    """
-    def __init__(self):
-        pass
-
     def _preprocess(self, df):
-        pass
+        df.query("pattern != 'C'", inplace=True)
+        df[COL_MODE] = df["pattern"].str[:1]
+        df[COL_LEVEL] = df["pattern"].str[1:].astype(int)
+        df.rename(columns={"song": COL_TITLE}, inplace=True)
+        return df
     
     def run(self):
         pass
