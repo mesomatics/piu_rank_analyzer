3ae08393728e5c970476607fb93f9cec39227b9b
mesomatics <68718172+mesomatics@users.noreply.github.com>
2024-03-17 21:47:21 +0900

    Update estimator.py

--- src/backend/estimator.py
+++ src/backend/estimator.py
@@ -1,3 +1,6 @@
+from scipy import stats
+import numpy as np
+
 from .cols import *
 
 
@@ -10,4 +13,21 @@ class CountEstimator:
         Apply Linear Distribution.
         """
         df[COL_COUNT] = len(df) - df[COL_RANK].rank() + 1
+        return df
+
+class NormalEstimator(CountEstimator):
+    """
+    Rank to Count
+    """
+    def __init__(self, min_max_ratio=100.):
+        self.min_max_ratio = min_max_ratio
+
+    def __call__(self, df):
+        """
+        Apply Normal Distribution.
+        """
+        quantiles = np.linspace(0, 1, len(df) + 2)[1:-1]
+        probs = stats.norm.ppf(quantiles)
+        shift = (probs[-1] - self.min_max_ratio * probs[0]) / (self.min_max_ratio - 1)
+        df[COL_COUNT] = np.flip(probs + shift)
         return df
